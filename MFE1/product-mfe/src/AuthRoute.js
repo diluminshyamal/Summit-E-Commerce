c44b1e2b36af21ea5bbf3eeb8e9ac0cc6e21e323
diluminshyamal <shyamal.desilva@sysco.com>
2025-03-27 14:08:03 +0530

    product services

--- MFE1/product-mfe/src/AuthRoute.js
+++ MFE1/product-mfe/src/AuthRoute.js
@@ -3,16 +3,14 @@ import { useNavigate } from "react-router-dom";
 
 const AuthRoute = ({ children }) => {
   const navigate = useNavigate();
-  const token = localStorage.getItem("authToken"); // Check for auth token
+  const token = localStorage.getItem("authToken");
 
   useEffect(() => {
-    // If the token is not present, redirect to the login page
     if (!token) {
       navigate("/login");
     }
   }, [token, navigate]);
 
-  // If token is present, render the children (protected route)
   return token ? children : null;
 };
 
