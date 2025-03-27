329f5f2487784e576467d33eb72c893109306bb4
diluminshyamal <shyamal.desilva@sysco.com>
2025-03-27 13:44:08 +0530

    auth route

--- MFE1/summit-app/src/root.component.js
+++ MFE1/summit-app/src/root.component.js
@@ -1,23 +1,51 @@
-// export default function Root(props) {
-//   return <section>{props.name} is mounted!</section>;
-// }
-
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import ProductPage from "./pages/ProductPage";
-import ProductDetails from "./components/ProductDetails/ProductDetails";
 import Header from "./components/Header/Header";
+import AuthRoute from "./AuthRoute";
+import HomeScreen from "./screens/HomeScreen";
+import ProductsView from "./components/ProductsView";
 
 export default function App() {
+  const userType = localStorage.getItem("userType");
+  const [approvePermission, setApprovePermission] = useState(false);
+  const [removePermission, setRemovePermission] = useState(false);
+
+  useEffect(() => {
+    if (userType == "steward") {
+      setApprovePermission(true);
+      setRemovePermission(true);
+    } else if (userType == "steward" || userType == "supplier") {
+      setRemovePermission(true);
+    }
+  }, []);
+
   return (
     <Router>
       <div>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<ProductPage />} />
-          <Route path="/product/:productId" element={<ProductDetails />} />
+          <Route
+            path="/"
+            element={
+              <AuthRoute>
+                <HomeScreen
+                  approvePermission={approvePermission}
+                  removePermission={removePermission}
+                />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/product/:productId"
+            element={
+              <AuthRoute>
+                <ProductsView
+                  approvePermission={approvePermission}
+                  removePermission={removePermission}
+                />
+              </AuthRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
